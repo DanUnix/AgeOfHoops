44dac885cf3c5312f48e1bde69f008ac1d5519c7
Daniel Pulley <Daniel Pulley>
2017-03-08 15:52:57 -0600

    Add Mesh Collider to HexMesh

--- Assets/Code/HexMesh.cs
+++ Assets/Code/HexMesh.cs
@@ -8,12 +8,14 @@ public class HexMesh : MonoBehaviour
     Mesh hexMesh;
     List<Vector3> vertices;
     List<int> triangles;
-    
+    MeshCollider meshCollider;
     void Awake()
     {
         
         
         GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+
         hexMesh.name = "Hex Mesh";
         vertices = new List<Vector3>();
         triangles = new List<int>();
