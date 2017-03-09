db89777160ccba2576efe05a6f9b233362770c78
YumeriaFreak <alanfan1@uic.edu>
2017-03-09 12:57:33 -0600

    added camera scripts

---
+++ Assets/Code/HexMapCamera.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMapCamera : MonoBehaviour {
+
+    Transform swivel, stick;
+    float zoom = 1f;
+    public float stickMinZoom = -320, stickMaxZoom = -100;
+    public float swivelMinZoom = 90, swivelMaxZoom = 45;
+    public float moveSpeedMinZoom = 500, moveSpeedMaxZoom = 200;
+
+    public HexGrid grid;
+
+    void Awake()
+    {
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta != 0f)
+        {
+            AdjustZoom(zoomDelta);
+        }
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+        if (xDelta != 0f || zDelta != 0f)
+        {
+            AdjustPosition(xDelta, zDelta);
+        }
+    }
+
+    void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+
+    void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction = new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom)
+            * damping * Time.deltaTime;
+
+        Vector3 position = transform.localPosition;
+        position += direction * distance;
+        transform.localPosition = position;
+    }
+}
