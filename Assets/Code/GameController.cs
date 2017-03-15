c668d5e08adbfcdf42d70c2a5a1c466292388b28
Daniel Pulley <Daniel Pulley>
2017-03-14 19:02:24 -0500

    Add Game Controller

---
+++ Assets/Code/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public HexGrid hexgrid;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (hexgrid.occupiedCells[(hexgrid.height / 2) * hexgrid.width] == true)
+        {
+
+        }
+	}
+}
