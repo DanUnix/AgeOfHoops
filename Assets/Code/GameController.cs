f198fbedb6df900fce0050207efa997c8db789d0
Daniel Pulley <Daniel Pulley>
2017-03-14 19:42:04 -0500

    Add win scene that activates once a player reaches a certain position

--- Assets/Code/GameController.cs
+++ Assets/Code/GameController.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
     public HexGrid hexgrid;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	void Start () {       
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (hexgrid.occupiedCells[(hexgrid.height / 2) * hexgrid.width] == true)
+        if (hexgrid.occupiedCells[((hexgrid.height / 2) * hexgrid.width) - 1] == true)
         {
-
+            SceneManager.LoadScene("Win", LoadSceneMode.Single);
         }
 	}
 }
