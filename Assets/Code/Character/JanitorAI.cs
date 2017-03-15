916c10dd276ded0b481aac402e4f400ad1780a8f
Daniel Pulley <Daniel Pulley>
2017-03-15 18:33:58 -0500

    Update janitor AI
    The AI for the Janitor lets the janitor character move around with an intial velocity
    
    If the janitor is interrupted by the player's mouse it increases its speed and moves in the opposite direction
    
    After 7.5 seconds of not being interrupted the janitor will then decrease its speed and return to its
    
    intial velocity.

--- Assets/Code/Character/JanitorAI.cs
+++ Assets/Code/Character/JanitorAI.cs
@@ -2,9 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+JanitorAI
+
+All Original Code
+
+The AI for the Janitor lets the janitor character move around with an intial velocity
+
+If the janitor is interrupted by the player's mouse it increases its speed and moves in the opposite direction
+
+After 7.5 seconds of not being interrupted the janitor will then decrease its speed and return to its
+
+intial velocity. 
+*/
 public class JanitorAI : MonoBehaviour {
 
-    public float movespeed = 100f;
+    private float movespeed;
 
     public Rigidbody rb;
 
@@ -12,9 +25,12 @@ public class JanitorAI : MonoBehaviour {
     private Vector3 upVector;
     private Vector3 rightVector;
 
+    private float coolDownTime;
+    private int timesSpedUp;
 	// Use this for initialization
 	void Start () {
-
+        timesSpedUp = 0;
+        movespeed = 100f;
         rb = GetComponent<Rigidbody>();
         upVector = new Vector3(0, 0, -1 * movespeed);
         rightVector = new Vector3(-1 * movespeed, 0, 0);
@@ -24,11 +40,6 @@ public class JanitorAI : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetMouseButtonDown(0)) {
-            rb.velocity *= -1;
-            reversed = reversed ? false : true;
-            
-        }
         if (transform.position.x <= -557 && transform.position.z >= 311)
         {
             rb.velocity = reversed ? -1 * rightVector : upVector;
@@ -44,5 +55,28 @@ public class JanitorAI : MonoBehaviour {
         else if (transform.position.x >= 557 && transform.position.z >= 311) {
             rb.velocity = reversed ? upVector : rightVector;
         }
+
+        coolDownTime -= Time.deltaTime;
+        if (coolDownTime <= 0 && timesSpedUp > 0) {
+            rb.velocity /= 2;
+            movespeed /= 2;
+            upVector = new Vector3(0, 0, -1 * movespeed);
+            rightVector = new Vector3(-1 * movespeed, 0, 0);
+
+            timesSpedUp -= 1;
+            if (timesSpedUp > 0) {
+                coolDownTime = 7.5f;
+            }
+        }
 	}
+
+    void OnMouseEnter() {
+        rb.velocity *= -2;
+        movespeed *= 2;
+        reversed = reversed ? false : true;
+        upVector = new Vector3(0, 0, -1 * movespeed);
+        rightVector = new Vector3(-1 * movespeed, 0, 0);
+        timesSpedUp += 1;
+        coolDownTime = 7.5f;
+    }
 }
