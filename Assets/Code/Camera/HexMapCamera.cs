a797059ef37b4909a9e448a33ec95f1d0214f370
Daniel Pulley <Daniel Pulley>
2017-03-14 19:55:23 -0500

    Clean up code directory
    Create folders to contain code related to each other.

---
+++ Assets/Code/Camera/HexMapCamera.cs
@@ -0,0 +1,89 @@
+﻿// Source: http://catlikecoding.com/unity/tutorials/hex-map/part-5/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMapCamera : MonoBehaviour {
+
+    Transform swivel, stick;
+    float zoom = 1f;
+    public float stickMinZoom = -320, stickMaxZoom = -100;
+    public float swivelMinZoom = 90, swivelMaxZoom = 45;
+    public float moveSpeedMinZoom = 500, moveSpeedMaxZoom = 200;
+    public float rotationSpeed = 180;
+    public float rotationAngle = 0;
+
+    public HexGrid grid;
+
+    void Awake()
+    {
+        swivel = transform.GetChild(0);
+        stick = swivel.GetChild(0);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if (zoomDelta != 0f)
+        {
+            AdjustZoom(zoomDelta);
+        }
+
+        float xDelta = Input.GetAxis("Horizontal");
+        float zDelta = Input.GetAxis("Vertical");
+        if (xDelta != 0f || zDelta != 0f)
+        {
+            AdjustPosition(xDelta, zDelta);
+        }
+        float rotationDelta = Input.GetAxis("Rotation");
+        if (rotationDelta != 0f)
+        {
+            AdjustRotation(rotationDelta);
+        }
+    }
+
+    void AdjustRotation(float delta)
+    {
+        rotationAngle += delta * rotationSpeed * Time.deltaTime;
+        if (rotationAngle < 0f)
+        {
+            rotationAngle += 360f;
+        }
+        else if (rotationAngle >= 360f)
+        {
+            rotationAngle -= 360f;
+        }
+        transform.localRotation = Quaternion.Euler(0f, rotationAngle, 0f);
+
+    }
+
+
+    void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom, swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
+    }
+
+
+    void AdjustPosition(float xDelta, float zDelta)
+    {
+        Vector3 direction = transform.localRotation * new Vector3(xDelta, 0f, zDelta).normalized;
+        float damping = Mathf.Max(Mathf.Abs(xDelta), Mathf.Abs(zDelta));
+        float distance = Mathf.Lerp(moveSpeedMinZoom, moveSpeedMaxZoom, zoom)
+            * damping * Time.deltaTime;
+
+        Vector3 position = transform.localPosition;
+        position += direction * distance;
+        transform.localPosition = position;
+    }
+}
