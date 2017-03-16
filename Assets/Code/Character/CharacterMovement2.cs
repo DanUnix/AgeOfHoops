837dd7cb38a53d6fe918ff88d0349c0f98fc3d38
YumeriaFreak <alanfan1@uic.edu>
2017-03-16 12:37:40 -0500

    added music, fixed character script

--- Assets/Code/Character/CharacterMovement2.cs
+++ Assets/Code/Character/CharacterMovement2.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CharacterMovement2 : MonoBehaviour {
 
     public HexGrid hexgrid;
-    private HexCell[] cells;
     private Color mouseOverColor = new Color(1, 0, 0, 0.4f);
     private Color originalColor;
     private bool dragging = false;
@@ -25,12 +24,14 @@ public class CharacterMovement2 : MonoBehaviour {
     
     // Use this for initialization
     void Start () {
-        cells = hexgrid.cells;
+        HexCell[] cells = hexgrid.cells;
         cellPositions = new List<Vector3>();
-        foreach (HexCell cell in cells)
+        foreach (HexCell c in cells)
         {
-            cellPositions.Add(cell.transform.position);
+            if (c != null) // skip the null cells (to make symmetrical board)
+                cellPositions.Add(c.transform.position);
         }
+        PinPosition();
     }
 
     void OnMouseEnter()
@@ -51,19 +52,33 @@ public class CharacterMovement2 : MonoBehaviour {
 
     void OnMouseUp()
     {
+        PinPosition();
+    }
+    // Update is called once per frame
+    void Update () {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = new Vector3(rayPoint.x, 150f, rayPoint.z);
+        }
+    }
+
+    void PinPosition()
+    {
         hexgrid.occupiedCells[oldIndex] = false;
         dragging = false;
-       
+
         Vector3 currentPosition = transform.position;
         globalPosition = currentPosition;
 
-        Vector3 closetCell = new Vector3(0,0,0);
+        Vector3 closetCell = new Vector3(0, 0, 0);
         float dist = float.MaxValue;
         int index = 0;
         for (int i = 0; i < cellPositions.Count; i++)
         {
             Vector3 v = cellPositions[i];
-        
+
             if (Vector3.Distance(v, currentPosition) < dist && hexgrid.occupiedCells[i] == false)
             {
                 dist = Vector3.Distance(v, currentPosition);
@@ -74,14 +89,5 @@ public class CharacterMovement2 : MonoBehaviour {
         transform.position = new Vector3(closetCell.x, 0f, closetCell.z);
         hexgrid.occupiedCells[index] = true;
         oldIndex = index;
-    }
-    // Update is called once per frame
-    void Update () {
-        if (dragging)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Vector3 rayPoint = ray.GetPoint(distance);
-            transform.position = new Vector3(rayPoint.x, 150f, rayPoint.z);
-        }
     }
 }
