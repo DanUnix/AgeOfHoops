9ee27a1eb19f61cc84c9217ecd66bd18f71969b7
thealanfan <alan.et.kate@gmail.com>
2017-03-30 12:30:50 -0500

    changed janitor AI to have physics/acceleration based movement

--- Assets/Code/Character/JanitorAI.cs
+++ Assets/Code/Character/JanitorAI.cs
@@ -18,13 +18,13 @@ The janitor takes a break every 20s when not interrupted and continues afterward
 public class JanitorAI : MonoBehaviour {
 
     private float movespeed;
+    private float acceleration;
 
     public Rigidbody rb;
 
     private bool reversed = false;
     private Vector3 upVector;
     private Vector3 rightVector;
-    private Vector3 preBreakVector;
 
     private float coolDownTime;
     private int timesSpedUp;
@@ -37,40 +37,47 @@ public class JanitorAI : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         timesSpedUp = 0;
-        movespeed = 100f;
+        movespeed = 50f;
+        acceleration = 0.5f;
+
         onBreak = false;
-        breakTimer = 20.0f;
+        breakTimer = 15.0f;
         restingTime = 0;
         rb = GetComponent<Rigidbody>();
         upVector = new Vector3(0, 0, -1 * movespeed);
         rightVector = new Vector3(-1 * movespeed, 0, 0);
-        rb.velocity = rightVector;
+        //rb.velocity = rightVector;
+        rb.AddForce(rightVector * rb.mass);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        JanitorBreak();
+        if (JanitorBreak())
+            return;
 
-        if (transform.position.x <= -557 && transform.position.z >= 311)
-        {
-            rb.velocity = reversed ? -1 * rightVector : upVector;
-        }
-        else if (transform.position.x <= -557 && transform.position.z <= -311)
-        {
-            rb.velocity = reversed ? -1 * upVector : -1 * rightVector;
-        }
-        else if (transform.position.x >= 557 && transform.position.z <= -311)
-        {
-            rb.velocity = reversed ? rightVector : -1 * upVector;
-        }
-        else if (transform.position.x >= 557 && transform.position.z >= 311) {
-            rb.velocity = reversed ? upVector : rightVector;
-        }
+        SetMovement();
+
+        //old code
+        //if (transform.position.x <= -557 && transform.position.z >= 311)
+        //{
+        //    rb.velocity = reversed ? -1 * rightVector : upVector;
+        //}
+        //else if (transform.position.x <= -557 && transform.position.z <= -311)
+        //{
+        //    rb.velocity = reversed ? -1 * upVector : -1 * rightVector;
+        //}
+        //else if (transform.position.x >= 557 && transform.position.z <= -311)
+        //{
+        //    rb.velocity = reversed ? rightVector : -1 * upVector;
+        //}
+        //else if (transform.position.x >= 557 && transform.position.z >= 311) {
+        //    rb.velocity = reversed ? upVector : rightVector;
+        //}
 
         coolDownTime -= Time.deltaTime;
         if (coolDownTime <= 0 && timesSpedUp > 0) {
-            rb.velocity /= 2;
+            //rb.velocity /= 2;
             movespeed /= 2;
             upVector = new Vector3(0, 0, -1 * movespeed);
             rightVector = new Vector3(-1 * movespeed, 0, 0);
@@ -83,37 +90,136 @@ public class JanitorAI : MonoBehaviour {
 	}
 
     void OnMouseEnter() {
-        rb.velocity *= -2;
-        movespeed *= 2;
+        //rb.velocity *= -2;
+        timesSpedUp += 1;
         reversed = reversed ? false : true;
+
+        if (timesSpedUp < 4)
+            movespeed *= 2;
+
         upVector = new Vector3(0, 0, -1 * movespeed);
         rightVector = new Vector3(-1 * movespeed, 0, 0);
-        timesSpedUp += 1;
         coolDownTime = 7.5f;
     }
 
-    void JanitorBreak()
+    bool JanitorBreak()
     {
         if (onBreak == false && timesSpedUp == 0)
         {
             breakTimer -= Time.deltaTime;
+            //time to go on break
+            if (breakTimer <= 0 && InCorner())
+            {
+                onBreak = true;
+                rb.velocity = new Vector3(0, 0, 0);
+                restingTime = 0f;
+                return true;
+            }
+            return false;
         }
-        if (breakTimer <= 0 && onBreak == false && timesSpedUp == 0)
-        {
-            onBreak = true;
-            preBreakVector = rb.velocity;
-            rb.velocity = new Vector3(0, 0, 0);
-            restingTime = 0f;
-        }
-        if (onBreak == true && restingTime <= 10.0f)
+        else if (onBreak == true && restingTime <= 8.0f)
         {
             restingTime += Time.deltaTime;
+            return true;
         }
-        else if (onBreak == true && restingTime > 10.0f) {
+        else if (onBreak == true && restingTime > 8.0f) {
             onBreak = false;
             restingTime = 0f;
-            rb.velocity = preBreakVector;
-            breakTimer = 20.0f;
+            breakTimer = 15.0f;
+            return false;
+        }
+        return false;
+    }
+
+    bool InCorner()
+    {
+        if (transform.position.x <= -557 && transform.position.z >= 311)
+        {
+            return true;
+        }
+        else if (transform.position.x <= -557 && transform.position.z <= -311)
+        {
+            return true;
+        }
+        else if (transform.position.x >= 557 && transform.position.z <= -311)
+        {
+            return true;
+        }
+        else if (transform.position.x >= 557 && transform.position.z >= 311)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void SetMovement()
+    {
+        //corners
+        if (transform.position.x <= -557 && transform.position.z >= 311)
+        {
+            rb.velocity = reversed ? -1 * rightVector : upVector;
+            return;
+        }
+        else if (transform.position.x <= -557 && transform.position.z <= -311)
+        {
+            rb.velocity = reversed ? -1 * upVector : -1 * rightVector;
+            return;
+        }
+        else if (transform.position.x >= 557 && transform.position.z <= -311)
+        {
+            rb.velocity = reversed ? rightVector : -1 * upVector;
+            return;
+        }
+        else if (transform.position.x >= 557 && transform.position.z >= 311)
+        {
+            rb.velocity = reversed ? upVector : rightVector;
+            return;
+        }
+
+        if (transform.position.x > -556 && transform.position.x <= 0)
+        {
+            if (transform.position.z <= -311)
+            {
+                rb.velocity += reversed ? new Vector3(acceleration, 0, 0) : new Vector3(acceleration, 0, 0);
+            }
+            else if (transform.position.z >= 311)
+            {
+                rb.velocity += reversed ? new Vector3(acceleration, 0, 0) : new Vector3(acceleration, 0, 0);
+            }
+        }
+        else if (transform.position.x > 0 && transform.position.x <= 556)
+        {
+            if (transform.position.z <= -311)
+            {
+                rb.velocity += new Vector3(-acceleration, 0, 0);
+            }
+            else if (transform.position.z >= 311)
+            {
+                rb.velocity += new Vector3(-acceleration, 0, 0);
+            }
+        }
+        else if (transform.position.z > -310 && transform.position.z <= 0)
+        {
+            if (transform.position.x <= -557)
+            {
+                rb.velocity += new Vector3(0, 0, acceleration);
+            }
+            else if (transform.position.x >= 557)
+            {
+                rb.velocity += new Vector3(0, 0, acceleration);
+            }
+        }
+        else if (transform.position.z > 0 && transform.position.z <= 310)
+        {
+            if (transform.position.x <= -557)
+            {
+                rb.velocity += new Vector3(0, 0, -acceleration);
+            }
+            else if (transform.position.x >= 557)
+            {
+                rb.velocity += new Vector3(0, 0, -acceleration);
+            }
         }
     }
 }
