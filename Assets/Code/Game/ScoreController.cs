4ec9015bec9a0056f9f6c1a00f5cdc1436e240e8
Daniel Pulley <Daniel Pulley>
2017-04-05 17:10:26 -0500

    Add ScoreController script
    Keeps track of player's score

---
+++ Assets/Code/Game/ScoreController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+
+    // Label for which the Score text will be updated per shot
+    public Text ScoreLabel;
+
+    // The Score Counter
+    public int Score;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set initial game score to 0
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Print Score to UI
+        ScoreLabel.text = "Score: " + Score;
+    }
+}
