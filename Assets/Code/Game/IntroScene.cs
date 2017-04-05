9feb91e4a963962752cecf0b0730528e9f90bbef
thealanfan <alan.et.kate@gmail.com>
2017-04-05 00:13:33 -0500

    changed Intro Scene code

--- Assets/Code/Game/IntroScene.cs
+++ Assets/Code/Game/IntroScene.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IntroScene : MonoBehaviour {
 
+    Text storyText;
 	// Use this for initialization
 	void Start () {
-		
+        storyText = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Code/Game/IntroSceneStory.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class IntroSceneStory : MonoBehaviour {
+
+    Text storyText;
+    private int counter;
+
+	// Use this for initialization
+	void Start () {
+        counter = 0;
+        storyText = GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.anyKeyDown)
+        {
+            switch (counter)
+            {
+                case 0:
+                    storyText.fontSize = 23;
+                    storyText.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+                    storyText.text = "\"Finally, I can be what I've always dreamed of,\" you thought...\n\n";
+                    break;
+                case 1:
+                    storyText.text = "...\"a basketball team owner!\"\n\n";
+                    break;
+                case 2:
+                    storyText.text = "end test \n\n(Press any key to continue)";
+                    SceneManager.LoadScene("AgeOfHoops", LoadSceneMode.Single);
+                    break;
+            }
+            ++counter;
+
+        }
+    }
+}
