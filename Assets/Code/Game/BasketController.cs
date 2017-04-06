3785f51d46e5c1d161485d3f572ac12ffdad8224
Daniel Pulley <Daniel Pulley>
2017-04-05 20:15:07 -0500

    Update BasketController script
    Add swish audio sound to hoop collision

--- Assets/Code/Game/BasketController.cs
+++ Assets/Code/Game/BasketController.cs
@@ -6,19 +6,24 @@ public class BasketController : MonoBehaviour {
 
     // reference the scoretext that will be incremented
     public ScoreController sc;
+    
+
+    AudioSource swishEffect;
 
     void OnCollisionEnter()
     {
-
+        
     }
 
-    void OnTriggerEnter()
+    void OnTriggerEnter(Collider other)
     {
+        
+        swishEffect.Play();
         sc.Score += 1;
     }
     // Use this for initialization
     void Start () {
-		
+        swishEffect = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
