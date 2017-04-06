05997b2825570c3cddaba58034f8db807340c1ff
thealanfan <alan.et.kate@gmail.com>
2017-04-06 13:21:02 -0500

    only valid if moving 1 away

--- Assets/Code/Character/CharacterMovement2.cs
+++ Assets/Code/Character/CharacterMovement2.cs
@@ -9,7 +9,7 @@ public class CharacterMovement2 : MonoBehaviour {
     private Color originalColor;
     private bool dragging = false;
     private float distance;
-    private int oldIndex = 0;
+    public int oldIndex;
 
     private List<Vector3> cellPositions;
     private Renderer renderer;
@@ -17,12 +17,14 @@ public class CharacterMovement2 : MonoBehaviour {
     public Vector3 globalPosition;
     public bool stayedInSameSpot;
 
+    public bool moved;
     public Animator animator;
 
     void Awake()
     {
         renderer = GetComponent<Renderer>();
         originalColor = renderer.material.color;
+        moved = false;
     }
     
     // Use this for initialization
@@ -63,7 +65,7 @@ public class CharacterMovement2 : MonoBehaviour {
         Vector3 newPosition = globalPosition;
         
         //set back to false in RoundCounter
-        if (oldPosition == newPosition)
+        if (oldPosition == newPosition && moved)
             stayedInSameSpot = true;    
     }
     // Update is called once per frame
@@ -78,6 +80,7 @@ public class CharacterMovement2 : MonoBehaviour {
 
     void PinPosition()
     {
+        moved = false;
         hexgrid.occupiedCells[oldIndex] = 0;
         dragging = false;
         animator.SetBool("Dragging", false);
@@ -98,10 +101,97 @@ public class CharacterMovement2 : MonoBehaviour {
                 index = i;
             }
         }
+
+
+        if (!validMove(index))
+        {
+            Vector3 oldPosition = cellPositions[oldIndex];
+            transform.position = new Vector3(oldPosition.x, 0f, oldPosition.z);
+            hexgrid.occupiedCells[oldIndex] = 1;
+            globalPosition = transform.position;
+            moved = false;
+            return;
+        }
+
+
         transform.position = new Vector3(closetCell.x, 0f, closetCell.z);
         hexgrid.occupiedCells[index] = 1;
         oldIndex = index;
 
         globalPosition = transform.position;
+        moved = true;
+    }
+
+
+    bool validMove(int newindex)
+    {
+        if (oldIndex == 0)
+        {
+            if (newindex == 0 || newindex == 1 || newindex == 8)
+                return true;
+        }
+        else if (oldIndex == 15)
+        {
+            if (newindex == 15 || newindex == 8 || newindex == 16 || newindex == 23)
+                return true;
+        }
+        else if (oldIndex == 30)
+        {
+            if (newindex == 30 || newindex == 31 || newindex == 23)
+                return true;
+        }
+        else if (oldIndex == 7)
+        {
+            if (newindex == 7 || newindex == 6 || newindex == 14)
+                return true;
+        }
+        else if (oldIndex == 22)
+        {
+            if (newindex == 22 || newindex == 21 || newindex == 14 || newindex == 37)
+                return true;
+        }
+        else if (oldIndex == 37)
+        {
+            if (newindex == 37 || newindex == 36 || newindex == 29)
+                return true;
+        }
+        else if ((oldIndex >= 8 && oldIndex <= 14) || ((oldIndex >= 23 && oldIndex <= 29)))
+        {
+            if (newindex == oldIndex || newindex == oldIndex + 8 || newindex == oldIndex + 7 ||
+                newindex == oldIndex - 7 || newindex == oldIndex - 8)
+                return true;
+
+            if (oldIndex == 8 && newindex == 9)
+                return true;
+            else if (oldIndex == 14 && newindex == 13)
+                return true;
+            else if (oldIndex == 23 && newindex == 24)
+                return true;
+            else if (oldIndex == 29 && newindex == 28)
+                return true;
+            else if ((oldIndex != 8 && oldIndex != 14 && oldIndex != 23 && oldIndex != 29)
+                && (oldIndex == newindex + 1 || oldIndex == newindex - 1))
+                return true;
+        }
+        else if (oldIndex >= 16 && oldIndex <= 21)
+        {
+            if (newindex == oldIndex || newindex == oldIndex + 8 || newindex == oldIndex + 7 ||
+                newindex == oldIndex - 7 || newindex == oldIndex - 8 ||
+                newindex == oldIndex + 1 || newindex == oldIndex - 1)
+                return true;
+        }
+        else if (oldIndex >= 1 || oldIndex <= 6)
+        {
+            if (newindex == oldIndex || newindex == oldIndex + 8 || newindex == oldIndex + 7 ||
+                newindex == oldIndex + 1 || newindex == oldIndex - 1)
+                return true;
+        }
+        else //31-36
+        {
+            if (newindex == oldIndex || newindex == oldIndex - 8 || newindex == oldIndex - 7 ||
+                newindex == oldIndex + 1 || newindex == oldIndex - 1)
+                return true;
+        }
+        return false;
     }
 }
