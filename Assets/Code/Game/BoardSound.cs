4d0d0a47898c6741387b7c3ce7fe59924f4e3a99
Jae Jeon <Jae Jeon>
2017-04-05 20:36:34 -0500

    added back board and hoop collider sound

---
+++ Assets/Code/Game/BoardSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardSound : MonoBehaviour {
+
+    // reference the scoretext that will be incremented
+    public ScoreController sc;
+
+
+    AudioSource boardEffect;
+
+    void OnCollisionEnter()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        boardEffect.Play();
+        sc.Score += 1;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        boardEffect = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
