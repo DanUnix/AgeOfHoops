faf62b198bac77c8e58c46b981c0e8406a6d05df
Daniel Pulley <Daniel Pulley>
2017-04-20 12:10:58 -0500

    Add Control GUI in game
    Added control text to pop up ingame when the i button is pressed.

---
+++ Assets/Code/Game/InGameInstruct.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameInstruct : MonoBehaviour {
+
+
+    public Canvas instructMenu;
+    public Button exitText;
+
+    // Use this for initialization
+    void Start () {
+        instructMenu = instructMenu.GetComponent<Canvas>();
+        instructMenu.enabled = false;
+        exitText = exitText.GetComponent<Button>();
+        exitText.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.I)) {
+            instructMenu.enabled = true;
+        }
+       
+    }
+
+    public void ExitPress()
+    {
+        instructMenu.enabled = true;
+        exitText.enabled = false;
+
+
+    }
+
+    public void noPress()
+    {
+        instructMenu.enabled = false;
+        exitText.enabled = true;
+
+    }
+}
